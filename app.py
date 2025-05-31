99740d1ed6c65dceabef3a45bcbfb0015a85f1c0
pengsim <hghvb024@gmail.com>
2025-05-31 13:19:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def add():
     cur.connection.commit()
     return jsonify({'message':'success'})
 if __name__ == '__main__':
-    app.run(host:'0.0.0.0' , port: 5000)
+    app.run(host='0.0.0.0' , port= 5000)
